467d03433698e020e327b586534b6e0b17ab7f7f
iampsrv <iampsrv@gmail.com>
2024-02-12 23:21:40 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# jenkins-project
+
---
+++ script.sh
@@ -0,0 +1,4 @@
+kubectl create -f namespace_prod.yaml
+kubectl create -f namespace_staging.yaml
+kubectl create -f service_prod.yaml
+kubectl create -f service_staging.yaml
